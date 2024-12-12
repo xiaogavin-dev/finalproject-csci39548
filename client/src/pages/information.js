3d15ffb3cc2432775d86e37c4f422b6c51b2d25c
xiaogavin-dev <gavin.xiao89@myhunter.cuny.edu>
2024-12-12 18:26:29 -0500

    fixed buttons on bottom

--- client/src/pages/information.js
+++ client/src/pages/information.js
@@ -7,23 +7,23 @@ const Information = () => {
         <div className='container_credits overflow-hidden'>
             <div class="top-center-text font-extrabold">Credits</div>
             <div className='bg-[#fceea7] relative top-[150px] min-h-[185px] max-w-[325px] justify-self-center rounded-lg'>
-            <div className='text-xl font-black flex text-center justify-center p-5'>
-                Ramisha Chowdhury<br></br>
-                Aaleia Fernado<br></br>
-                Maya Jamaddar<br></br>
-                Michelle Wen<br></br>
-                Gavin Xiao<br></br>
+                <div className='text-xl font-black flex text-center justify-center p-5'>
+                    Ramisha Chowdhury<br></br>
+                    Aaleia Fernado<br></br>
+                    Maya Jamaddar<br></br>
+                    Michelle Wen<br></br>
+                    Gavin Xiao<br></br>
+                </div>
             </div>
-
-            </div>
-
-            <a href='https://pokeapi.co' className='flex justify-center top-[600px] right-[75px] relative'>
+            <div className='flex justify-center'>
+            <a href='https://pokeapi.co' className='top-[600px] relative max-w-[125px] right-[75px]'>
                 <img src={pokeapi} alt='pokeapi logo' className='w-[125px] h-[75px]' />
             </a>
 
-            <a href='https://github.com/xiaogavin-dev/finalproject-csci39548' className='flex justify-center relative top-[525px] left-[75px]'>
+            <a href='https://github.com/xiaogavin-dev/finalproject-csci39548' className='relative top-[600px] max-w-[75px] left-[25px]'>
                 <img src={github} alt='github logo' className='w-[75px] h-[75px]' />
             </a>
+            </div>
         </div>
     );
 }
